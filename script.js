e023b6ed89552d5cc66afa6a6a5cb62b801dc8b0
clarakk <claracav@gmail.com>
2023-05-16 15:43:50 +0200

    new navigation menu

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
+function myFunction() {
+  // document.getElementById("competence").style.display = "block";
+  // document.getElementById("competence2").style.display = "block";
+  //  document.getElementById("competence3").style.display = "block";
+  // alert("che figa che so")
+  // document.getElementsByClassName("navlinks").style.display = "block";
+    document.getElementById("competences").style.display = "block";
+     document.getElementById("competences").style.margin = "auto";
+  document.getElementsByTagName("li")[0].style.display = "block";
+  document.getElementsByTagName("li")[1].style.display = "block";
+  document.getElementsByTagName("li")[2].style.display = "block";
+  document.getElementById("icon").style.display = "none";
+}
 
-function myFunction()  {
-document.getElementsByClassName(".my-competences").style.display = "block";
-// alert("che figa che so")
-};
-
+myfunction();
